c2942835fd134df267877980555ec798bf88ecad
dengpengfei <dengpengfei@hetao101.com>
2026-05-21 20:04:46 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
+import readline from "readline";
 import { agentLoop } from "./agent";
 
-await agentLoop(
-  // "我需要网页贪食蛇游戏，在output目录下创建一个snake.html文件"
-  // 我需要网页3d飞行游戏，在output目录下创建一个index.html文件，分段写代码
-  // "我需要网页俄罗斯方块游戏，在output目录下创建一个tetris.html文件",
-  // "查询天气",
-  "生成一个绚丽的星空特效，在output目录下创建一个starry-sky.html文件",
-  // "今天财经新闻",
-);
-// curl "wttr.in/pingdingshan?format=j1&lang=zh-cn"
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const prompt = await new Promise<string>((resolve) => {
+  rl.question("🧑 请输入：", (ans) => {
+    rl.close();
+    resolve(ans.trim());
+  });
+});
+
+if (!prompt) {
+  console.log("未输入，退出。");
+  process.exit(0);
+}
+
+await agentLoop(prompt);
