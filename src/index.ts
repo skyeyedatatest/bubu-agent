cec66e490182e35281880e019d5ec8320f338b27
dengpengfei <dengpengfei@hetao101.com>
2026-05-21 19:49:27 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ await agentLoop(
   // "我需要网页贪食蛇游戏，在output目录下创建一个snake.html文件"
   // 我需要网页3d飞行游戏，在output目录下创建一个index.html文件，分段写代码
   // "我需要网页俄罗斯方块游戏，在output目录下创建一个tetris.html文件",
-  "查询天气",
+  // "查询天气",
+  "生成一个绚丽的星空特效，在output目录下创建一个starry-sky.html文件",
   // "今天财经新闻",
 );
 // curl "wttr.in/pingdingshan?format=j1&lang=zh-cn"
